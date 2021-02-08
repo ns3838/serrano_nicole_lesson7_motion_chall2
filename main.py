2426da3936de92d04d5885dea8c8c0ed987cbfbe
ns3838 <72807027+ns3838@users.noreply.github.com>
2021-02-08 00:49:47 -0500

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-
+#Make the device read rotational input and create an output of sound, light, or color 
+print:
+input.rotation(Rotation.PITCH) 
+input.rotation(Rotation.ROLL)
--- main.ts
+++ main.ts
@@ -1,1 +1,4 @@
-
+// Make the device read rotational input and create an output of sound, light, or color 
+console.log
+input.rotation(Rotation.Pitch)
+input.rotation(Rotation.Roll)
