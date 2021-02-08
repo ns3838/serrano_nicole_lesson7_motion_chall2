ba47e5d6f7940a340c14aef9c515b2aab2fe637e
ns3838 <72807027+ns3838@users.noreply.github.com>
2021-02-08 01:02:24 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #Make the device read rotational input and create an output of sound, light, or color 
-print:
-input.rotation(Rotation.PITCH) 
-input.rotation(Rotation.ROLL)
+while True:
+    print ("Rotation of degrees in x-axis: " + input.rotation(Rotation.PITCH))
+    print (" Rotation of degrees in y-axis: " + input.rotation(Rotation.ROLL))
+    if input.rotation(Rotation.PITCH) > 0:
+        light.set_all(light.rgb(255,0,255))
--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
 // Make the device read rotational input and create an output of sound, light, or color 
-console.log
-input.rotation(Rotation.Pitch)
-input.rotation(Rotation.Roll)
+while (true) {
+    console.log("Rotation of degrees in x-axis: " + input.rotation(Rotation.Pitch))
+    console.log(" Rotation of degrees in y-axis: " + input.rotation(Rotation.Roll))
+    if (input.rotation(Rotation.Pitch) > 0) {
+        light.setAll(light.rgb(255, 0, 255))
+    }
+    
+}
